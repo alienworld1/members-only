9a3fc5e5560679e22613a4c793ae944fda28cb21
alienworld1 <69631697+alienworld1@users.noreply.github.com>
2024-06-18 20:58:06 +0530

    Post messages on submitting the message form

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,9 +1,44 @@
 const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
+
+const Message = require('../models/message');
 
 exports.messages_get = asyncHandler(async(req, res) => {
   res.render('messages', {title: 'Messages'});
 });
 
 exports.messages_create_get = asyncHandler(async(req, res) => {
-  res.render('message_form', {title: 'Write a Message'});
+  res.render('message_form', {title: 'Post a Message'});
 });
+
+exports.messages_create_post = [
+  body('title', 'The title must be specified')
+    .trim()
+    .isLength({min: 1})
+    .escape(),
+  body('text', 'The message length must be between 1-280 characters')
+    .trim()
+    .isLength({min: 1, max: 280})
+    .escape(),
+  
+  asyncHandler(async(req, res) => {
+    const errors = validationResult(req);
+
+    const message = new Message({
+      title: req.body.title,
+      text: req.body.text,
+      author: req.user.id,
+      timestamp: new Date(),
+    });
+
+    if (!errors.isEmpty()) {
+      res.render('message_form', {
+        title: 'Post a Message',
+        errors: errors.array(),
+      });
+    } else {
+      await message.save();
+      res.redirect('/messages');
+    }
+  })
+];
--- routes/messages.js
+++ routes/messages.js
@@ -4,5 +4,6 @@ const messageController = require('../controllers/messageController');
 router.get('/', messageController.messages_get);
 
 router.get('/create', messageController.messages_create_get);
+router.post('/create', messageController.messages_create_post);
 
 module.exports = router;
